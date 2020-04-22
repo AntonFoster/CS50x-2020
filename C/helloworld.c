477ae71e27ce7b7a0e8df73c52a37a83e3a7e4e8
Anton Foster <antonf@i2csystems.com>
2020-04-22 17:19:17 +0200

    Added my C files

---
+++ C/helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void){
+//  char answer[];
+//  answer = "Anton Foster";
+// = get_string("Wht is your name ?\n");
+//  printf("Hello World %s \n", answer);
+    char name[30];
+    int age;
+    printf("Enter your name : \n");
+    scanf("%s" , name); 
+    printf("You entered: %s \n", name );
+}
